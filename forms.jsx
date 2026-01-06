5cbb45db6cb80689cf8f50ed22410f4868d1dab6
sanjay <sasanjay384@gmail.com>
2026-01-06 10:38:53 +0530

    program 3 react lab

---
+++ forms.jsx
@@ -0,0 +1,44 @@
+import React,{useState} from 'react'
+
+function forms() {
+    const [name,setname]=useState("")
+    const [email,setemail]=useState("")
+    const [password,setpassword]=useState("")
+    const [submittedData,setsubmittedData]=useState("")
+    const handlesubmit=(e)=>{
+    e.preventDefault();
+    const formdata={name,email,password}
+    setsubmittedData(formdata);
+    setname("")
+    setemail("")
+    setpassword("")
+
+
+}
+  return (
+    <div>
+    <h2>form</h2>
+    <form onSubmit={handlesubmit}>
+        <label>name:</label>
+        <input type="text"
+        value={name}
+        onChamge={(e)=>setname(e.target.value)}
+        />
+        <label>email:</label>
+        <input type="email"
+        value={email}
+        onChamge={(e)=>setemail(e.target.value)}
+        />
+        <label>password:</label>
+        <input type="password"
+        value={password}
+        onChamge={(e)=>setpassword(e.target.value)}
+        />
+        <button type="submit">submit</button>
+
+    </form>
+    </div>
+  )
+}
+
+export default forms
